fab7fbc22dbda09b1da02c37791dd18dc11eff43
Rico Huijbers <rix0rrr@gmail.com>
2015-10-19 21:19:16 +0000

    Make assembly link on GCC as well

--- src/libbruce/src/be/disk.cpp
+++ src/libbruce/src/be/disk.cpp
@@ -5,11 +5,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-extern "C" {
-
-void sha1_compress(uint32_t state[5], const uint8_t block[64]);
-
-}
+void sha1_compress(uint32_t state[5], const uint8_t block[64]) asm ("sha1_compress");
 
 void sha1_hash(const uint8_t *message, uint32_t len, uint32_t hash[5])
 {
